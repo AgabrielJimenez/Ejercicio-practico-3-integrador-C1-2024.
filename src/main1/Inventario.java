95c7eefc1a6d806296f3502767d1b6d4313e0e0b
AgabrielJimenez <156978389+AgabrielJimenez@users.noreply.github.com>
2024-03-20 20:47:08 -0600

    Add files via upload

---
+++ src/main1/Inventario.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main1;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+
+import javax.swing.JOptionPane;
+
+public class Inventario {
+
+    private Producto[] productos;
+    private int cantidadProductos;
+
+    public Inventario(int capacidad) {
+        productos = new Producto[capacidad];
+        cantidadProductos = 0;
+    }
+
+    public void agregarProducto(Producto producto) {
+        if (cantidadProductos < productos.length) {
+            productos[cantidadProductos] = producto;
+            cantidadProductos++;
+        } else {
+            JOptionPane.showMessageDialog(null, "Inventario lleno, no se pueden agregar más productos.");
+        }
+    }
+
+    public void mostrarProductos() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < cantidadProductos; i++) {
+            sb.append(productos[i].toString());
+        }
+        JOptionPane.showMessageDialog(null, sb.toString());
+    }
+
+    public double calcularTotalGanancia() {
+        double totalGanancia = 0;
+        for (int i = 0; i < cantidadProductos; i++) {
+            totalGanancia += productos[i].getPrecioTotal();
+        }
+        return totalGanancia;
+    }
+}
---
+++ src/main1/Producto.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main1;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+import javax.swing.JOptionPane;
+
+public class Producto {
+
+    private String codigo;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+
+    public Producto(String codigo, String nombre, double precio, int cantidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioTotal() {
+        return precio * cantidad;
+    }
+
+    public String toString() {
+        return "Código: " + codigo + "\n"
+                + "Nombre: " + nombre + "\n"
+                + "Precio: " + precio + "\n"
+                + "Cantidad: " + cantidad + "\n"
+                + "Precio Total: " + getPrecioTotal() + "\n";
+    }
+}
